a88405afe528e17597b91409c1a8f6d14e306617
Quoc An Duong <quocand40@gmail.com>
2022-12-14 22:06:22 +0100

    add : create GRPD for register page

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -3,6 +3,8 @@ import { clearPage } from '../../utils/render';
 import Navbar from '../Navbar/Navbar';
 import Navigate from '../Router/Navigate';
 
+const Swal = require('sweetalert2')
+
 const RegisterPage = () => {
   clearPage();
   renderRegisterForm();
@@ -78,8 +80,11 @@ function renderRegisterForm() {
   checkLabel.htmlFor = 'rememberme';
   checkLabel.className = 'form-check-label';
   checkLabel.textContent = 'Remember me';
-
-
+  
+  const rgpd = `<p id = "rgpd">
+  By registering, you confirm that you have read, understood and accept the <div id = "termOfUse"><p>
+  privacy policy<p></div>
+  </p>`
   // Create paragraph for error message 
   const errorMessage = document.createElement('p');
   errorMessage.id = 'errorMessage';
@@ -88,7 +93,8 @@ function renderRegisterForm() {
   // Appenchild remember me + his label to the wrapper
   formCheckWrapper.appendChild(rememberme);
   formCheckWrapper.appendChild(checkLabel);
-
+  formCheckWrapper.innerHTML += rgpd;
+  
   // Appenchild all field from the form to the form
   form.appendChild(title);
   form.appendChild(username);
@@ -103,6 +109,26 @@ function renderRegisterForm() {
 
   // Appenchild div to main
   main.appendChild(formDiv);
+  const rgpdDoc = document.getElementById('termOfUse');
+  const textRgpd = `<strong>ARTICLE 1: PREAMBLE</strong ><br>
+  Information to minors: We keep the information you declare to us (a nickname) in order to be able to communicate with you about your account, solve security problems that could affect your account. When you create your account, we ask you to enter an account name and a password: this account name and password will allow you to access and create a new quiz. Please note: this account name and password must remain secret.
+  You do not have the right to access and rectify your data. To exercise this right and obtain communication or deletion of your information, write to us, with the help of your parents, by email, quizTimerGame@gmail.com.<br>
+   <strong>ARTICLE 2: DATA RETENTION</strong><br>  
+   We keep your personal data for as long as necessary for the fulfilment of the purposes set out herein, in compliance with the legislation in force. To know your rights to erasure, please consult the article 'Your rights'.<br>
+   <strong>ARTICLE 3: SECURITY</strong><br>
+   The Company implements the appropriate technical and organizational measures, with regard to the nature of the data and the risks presented by the processing, to guarantee a high level of data security and, in particular, to prevent them from being distorted, damaged or accessed by unauthorized third parties."
+   
+   To this end, md5 technology is used for the encryption of certain data, physical and logical data backup procedures are implemented and a protocol is used to secure access through data encryption (HTTPS)."
+   
+   The Company reserves the right to take legal action against any person who attempts to access personal information that does not belong to the Company.`
+  rgpdDoc.addEventListener('click', () => {
+    Swal.fire({
+      title: 'privacy policy',
+      html : textRgpd
+
+    })
+  })
+
   form.addEventListener('submit', onRegister);
 }
 
