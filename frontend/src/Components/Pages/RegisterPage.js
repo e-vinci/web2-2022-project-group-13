7a90c37d02602be7ae1412138766803361353461
Quoc An Duong <quocand40@gmail.com>
2022-12-15 23:40:54 +0100

    add checkbox for grpd

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -57,7 +57,7 @@ function renderRegisterForm() {
   submit.id = 'submitButton';
   submit.value = 'Register';
   submit.type = 'submit';
-  submit.className = 'btn btn-primary';
+  submit.className = 'btn btn-primary disabled';
 
   // Create div wrapper for checkbox + his label
   const formCheckWrapper = document.createElement('div');
@@ -80,10 +80,25 @@ function renderRegisterForm() {
   checkLabel.className = 'form-check-label';
   checkLabel.textContent = 'Remember me';
 
-  const rgpd = `<p id="rgpd">
+  // create rgpdBox
+  const divRpgdWrapper = document.createElement('div');
+  divRpgdWrapper.className = 'mb-3 form-check';
+  divRpgdWrapper.id = 'checkBoxWrapper';
+  
+  const rgpdBox = document.createElement('input');
+  rgpdBox.type = 'checkbox';
+  rgpdBox.className = 'form-check-input';
+  rgpdBox.id = 'rgpdBox';
+  rgpdBox.addEventListener('click', onCheckBoxRgpdClicked);
+
+  const checkRgpdLabel = document.createElement('label');
+  checkRgpdLabel.id = 'labelForCheckbox';
+  checkRgpdLabel.className = 'form-check-label';
+  checkRgpdLabel.innerHTML += `<p id="rgpd">
   By registering, you confirm that you have read, understood and accept the <div id = "termOfUse"><p>
   Privacy policy<p></div>
   </p>`;
+
   // Create paragraph for error message
   const errorMessage = document.createElement('p');
   errorMessage.id = 'errorMessage';
@@ -92,7 +107,9 @@ function renderRegisterForm() {
   // Appenchild remember me + his label to the wrapper
   formCheckWrapper.appendChild(rememberme);
   formCheckWrapper.appendChild(checkLabel);
-  formCheckWrapper.innerHTML += rgpd;
+  divRpgdWrapper.appendChild(rgpdBox);
+  divRpgdWrapper.appendChild(checkRgpdLabel);
+  formCheckWrapper.appendChild(divRpgdWrapper);
 
   // Appenchild all field from the form to the form
   form.appendChild(title);
@@ -132,6 +149,18 @@ function renderRegisterForm() {
 
 function onCheckboxClicked(e) {
   setRememberMe(e.target.checked);
+}
+
+function onCheckBoxRgpdClicked(e){
+  if (e.target.checked){
+    const RegisterButtonEnable = document.getElementById('submitButton');
+    RegisterButtonEnable.className = RegisterButtonEnable.className.replace('disabled', '');
+  }
+  else{
+    const RegisterButtonEnable = document.getElementById('submitButton');
+    RegisterButtonEnable.className += ' disabled';
+  }
+  
 }
 
 async function onRegister(e) {
